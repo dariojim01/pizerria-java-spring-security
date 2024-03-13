60cf062a97993e8a396e706ebbc34df0e028f104
Dario Jimenez <dariojim01@gmail.com>
2024-03-12 21:46:51 -0500

    HttpBasic and requestMatchers

---
+++ src/main/java/com/pizza/pizzeria/web/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.pizza.pizzeria.web.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .cors().and()
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                .requestMatchers(HttpMethod.PUT).denyAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        
+        return http.build();
+    }
+}
