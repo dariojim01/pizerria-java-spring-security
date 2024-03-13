6d93b9ea837a4981b7fcc4bdb1179aeabceee5f7
Dario Jimenez <dariojim01@gmail.com>
2024-03-12 23:04:28 -0500

    Users and roles

---
+++ src/main/java/com/pizza/pizzeria/service/UserSecurityService.java
@@ -0,0 +1,55 @@
+package com.pizza.pizzeria.service;
+
+import com.pizza.pizzeria.persistence.entity.UserEntity;
+import com.pizza.pizzeria.persistence.entity.UserRoleEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserSecurityService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Autowired
+    public UserSecurityService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = this.userRepository.findById(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        String[] roles = userEntity.getRoles().stream().map(UserRoleEntity::getRole).toArray(String[]::new);
+        return UserEntity.builder()
+                .username(userEntity.getUsername())
+                .password(userEntity.getPassword())
+                .authorities(this.grantedAuthorities(roles))
+                .accountLocked(userEntity.getLocked())
+                .disabled(userEntity.getDisabled())
+                .build();
+
+    }
+    private String[] getAuthorities(String role){
+        if("ADMIN".equals(role)|| "CUSTOMER".equals(role)) {
+            return new String[]{"random_order"};
+        }
+        return new String[]{};
+    }
+    public List<GrantedAuthority> grantedAuthorities(String[] roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>(roles.length);
+        for(String role : roles) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_"+role));
+            for(String authority : this.getAuthorities(role)) {
+                authorities.add(new SimpleGrantedAuthority(authority));
+            }
+        }
+        return authorities;
+    }
+
+}
--- src/main/java/com/pizza/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/pizza/pizzeria/web/config/SecurityConfig.java
@@ -5,17 +5,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
-                .cors().and()
+                //.csrf().disable()
+                //.cors().and()
                 .authorizeHttpRequests()
                 .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
-                .requestMatchers(HttpMethod.PUT).denyAll()
+                .requestMatchers(HttpMethod.POST, "/api/pizzas/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT).hasRole("ADMIN")
+                .requestMatchers("/api/orders/random").hasAuthority("random_order")
+                .requestMatchers("/api/users/**").hasRole("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -23,4 +28,9 @@ public class SecurityConfig {
         
         return http.build();
     }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
+
